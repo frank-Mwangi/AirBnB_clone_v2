21ca87d52f640641333f89b052705bc3cdd8a4f0
frank-Mwangi <kungofrank@gmail.com>
2023-05-08 23:58:56 +0300

    Deploy archive file to remote servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,37 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
-from fabric.api import *
-from datetime import datetime
-from os import path
+"""Deploy an archive to remote webservers."""
+
+
+from fabric.api import env, put, run
+import os
 
 
 env.hosts = ['100.25.103.66', '100.25.215.1']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Deploy web files to server
     """
-    try:
-        if not (path.exists(archive_path)):
-            return False
+    Uploads an archive file to remote web servers.
 
-        # upload archive
-        put(archive_path, '/tmp/')
+    args:
+        archive_path: path to the archive file
+    Returns:
+        True if success, false otherwise
 
-        # create target directory
-        timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-        # uncompress archive and delete .tgz
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
-
-        # remove archive
-        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-        # move contents into host web_static
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-        # remove extraneous web_static dir
-        run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-            .format(timestamp))
-
-        # delete pre-existing sym link
-        run('sudo rm -rf /data/web_static/current')
-
-        # recreate symbolic link
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-    except:
+    """
+    if not os.path.exists(archive_path):
         return False
-
-    # return True on success
+    put(archive_path, "/tmp")
+    dirName = archive_path.split('/')[1].split('.')[0]
+    run("mkdir -p /data/web_static/releases/{}".format(dirName))
+    archiveName = archive_path.split('/')[1]
+    run("tar -xzf /tmp/{} -C"
+        " /data/web_static/releases/{}/".format(archiveName, dirName))
+    run("rm /tmp/{}".format(archiveName))
+    run("mv /data/web_static/releases/{}/web_static/*"
+        " /data/web_static/releases/{}/".format(dirName, dirName))
+    run("rm -rf /data/web_static/releases/{}/web_static".format(dirName))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{}/"
+        " /data/web_static/current".format(dirName))
     return True
