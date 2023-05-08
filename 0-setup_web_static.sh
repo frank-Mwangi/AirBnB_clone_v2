61ea6376f07a5620ca1eb56c82b5d2fe83f661d4
frank-Mwangi <kungofrank@gmail.com>
2023-05-08 21:14:39 +0300

    Initial server setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,13 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 sudo chmod go+w /data/web_static/releases/test/index.html
-echo "AirBnB testing" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
